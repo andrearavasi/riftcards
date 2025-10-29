1dd3cd839c4ae14dee60fa57ce23ef6ee36d0ca4
Andrea Ravasi <a.ravasi@carpinera.it>
2025-10-29 12:44:18 +0100

    feat: rename project to my-cards and update adapter configuration for static site generation

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 // import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -12,7 +12,17 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			// La cartella di output predefinita è 'build'
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html', // O '404.html' se preferisci
+			precompress: false,
+			strict: true
+		}),
+		paths: {
+			base: dev ? '' : '/riftcards',
+		}
 	}
 };
 
